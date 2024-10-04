b00b6c226a979c15036e700ff8f3bbac77e2a0bd
devtomschi <36606220+devtomschi@users.noreply.github.com>
2024-10-04 11:14:21 +0200

    Fix NVidia header installation (#9)

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,8 +18,9 @@ export CXXFLAGS="$CXXFLAGS -I$SRC_DIR/clr/opencl/khronos/headers/opencl2.2/"
 
 install $SRC_DIR/clr/rocclr/platform/prof_protocol.h $PREFIX/include
 
+install -d $PREFIX/include/hip/nvidia_detail
 for f in $SRC_DIR/hipother/hipnv/include/hip/nvidia_detail/*.h; do \
-    install -D -t $PREFIX/include/hip/nvidia_detail/ $f; \
+    install $f $PREFIX/include/hip/nvidia_detail ; \
 done
 
 cmake -LAH \
